6f5c2c9314c865f960b1ebe6808ac649448d5499
Luke Cleghorn <lcleghorn@agl.com.au>
2018-07-05 10:59:25 +1000

    Grease Monkey Script

---
+++ Hackathon.MightyDucks.Gamify/GreaseMonkeyPointsSender.js
@@ -0,0 +1,12 @@
+﻿// ==UserScript==
+// @name     Mighty Ducks - Points Sender
+// @version  1
+// @grant    none
+// @require http://ajax.googleapis.com/ajax/libs/jquery/1.9.1/jquery.min.js
+// ==/UserScript==
+window.addEventListener('load', function() {
+    console.log("I am working");
+    $.get(
+        "https://mightyduckshackatongamify.azurewebsites.net/api/points/send",
+        {id : "1", points : 500});
+}, false);
