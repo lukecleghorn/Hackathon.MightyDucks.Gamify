69cb6a78db38c5a88b0d874a615a286e35ea835a
Luke Cleghorn <lcleghorn@agl.com.au>
2018-07-05 11:36:09 +1000

    Working send data grease monkey script

--- Hackathon.MightyDucks.Gamify/GreaseMonkeyPointsSender.js
+++ Hackathon.MightyDucks.Gamify/GreaseMonkeyPointsSender.js
@@ -4,9 +4,18 @@
 // @grant    none
 // @require http://ajax.googleapis.com/ajax/libs/jquery/1.9.1/jquery.min.js
 // ==/UserScript==
-window.addEventListener('load', function() {
+$(document).ready(function() {
+    $('a[href*="/solar-renewables/solar-energy"]').click(function(e) {
+        e.preventDefault();
+
+        console.log("clicked");
+        sendData();
+        return false;
+    });
+});
+function sendData() {
     console.log("I am working");
     $.get(
         "https://mightyduckshackatongamify.azurewebsites.net/api/points/send",
-        {id : "1", points : 500});
-}, false);
+        { id: "1", points: 500 });
+}
